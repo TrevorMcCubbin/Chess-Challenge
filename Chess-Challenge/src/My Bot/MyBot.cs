ab3040953140fb573dd2e97e8466ae77b7fbdb1c
littlett98 <mccubal13@gmail.com>
2023-07-25 20:21:31 -0400

    Add Check for Checkmate

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -5,6 +5,21 @@ public class MyBot : IChessBot
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
+        foreach (var move in moves)
+        {
+            if (MoveIsCheckmate(board, move))
+            {
+                return move;
+            }
+        }
         return moves[0];
+    }
+
+    private bool MoveIsCheckmate(Board board, Move move)
+    {
+        board.MakeMove(move);
+        bool isMate = board.IsInCheckmate();
+        board.UndoMove(move);
+        return isMate;
     }
 }
