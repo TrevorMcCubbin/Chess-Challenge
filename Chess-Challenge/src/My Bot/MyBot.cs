1b46d65723b3ea7442ca460e083a96e20b681c92
littlett98 <mccubal13@gmail.com>
2023-07-25 21:16:56 -0400

    Add capturing free pawns

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -2,24 +2,44 @@
 
 public class MyBot : IChessBot
 {
+    // Piece values: null, pawn, knight, bishop, rook, queen, king
+    private readonly int[] PIECE_VALUES = { 0, 100, 300, 300, 500, 900, 10000 };
+    int highestValueCapture = 0;
+
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
+        highestValueCapture = 0;
+        var moves = board.GetLegalMoves();
         var moveToMake = moves[0];
         foreach (var move in moves)
         {
+            // Always play checkmate in one move
             if (MoveIsCheckmate(board, move))
             {
                 return move;
             }
-            // Don't move to a square that is under attack
-            if (board.SquareIsAttackedByOpponent(move.TargetSquare))
+
+            if (CapturedPieceIsOfHigherValue(highestValueCapture, board, move))
             {
-                continue;
+                    highestValueCapture = PIECE_VALUES[(int)board.GetPiece(move.TargetSquare).PieceType];
+                    moveToMake = move;
             }
-            moveToMake = move;
         }
         return moveToMake;
+    }
+
+    private bool CapturedPieceIsOfHigherValue(int highestValueCapture, Board board, Move move)
+    {
+        var capturedPiece = board.GetPiece(move.TargetSquare);
+
+        // for this iteration only take unguarded pieces
+        if (board.SquareIsAttackedByOpponent(capturedPiece.Square))
+        {
+            return false;
+        }
+
+        var capturedPieceValue = PIECE_VALUES[(int)capturedPiece.PieceType];
+        return capturedPieceValue > highestValueCapture;
     }
 
     private bool MoveIsCheckmate(Board board, Move move)
