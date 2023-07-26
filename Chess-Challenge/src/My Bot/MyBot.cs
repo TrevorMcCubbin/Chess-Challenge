fa56b2927a4c5f8c61955fe3d3bb171c54470cfe
littlett98 <mccubal13@gmail.com>
2023-07-25 20:32:00 -0400

    Add don't move to a square that is under attack

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -5,14 +5,21 @@ public class MyBot : IChessBot
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
+        var moveToMake = moves[0];
         foreach (var move in moves)
         {
             if (MoveIsCheckmate(board, move))
             {
                 return move;
             }
+            // Don't move to a square that is under attack
+            if (board.SquareIsAttackedByOpponent(move.TargetSquare))
+            {
+                continue;
+            }
+            moveToMake = move;
         }
-        return moves[0];
+        return moveToMake;
     }
 
     private bool MoveIsCheckmate(Board board, Move move)
